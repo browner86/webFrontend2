f8914c815fe606f3b24db7f9235af5b69c057e7a
Brian Brown <browner86@gmail.com>
2021-04-23 15:51:30 -0400

    testing innerHtml

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const links = [
 // });
 // let linkHtml = document.querySelector(".linkHtml");
 // linkHtml.textContent = 'Hello World';
-var x = document.getElementById("myDIV");
-x.querySelector(".links").innerHTML = "Hello World!";
-x.querySelector("#paragraph").innerHTML = "This is a paragraph";
+// var x = document.getElementById("myDIV");
+// x.querySelector(".links").innerHTML = "Hello World!";
+// x.querySelector("#paragraph").innerHTML = "This is a paragraph";
+document.getElementById('hello').innerHTML = "Hello World";

8f0a2bd273a44c76ecf879e86cf121a8eb80462c
Brian Brown <browner86@gmail.com>
2021-04-23 18:44:00 -0400

    fix broken div id in main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,32 @@
+/*
+Generates html for index links variable will be updated weekly to
+create a new link to that weeks project.
+*/
+
+//create links array. each iterator is object
+//key value pairs with label and url
 const links = [
   { label: "Week1 notes",
     url: "week1/index.html"
-  }
-  // ,
-  // {
-  //   label: "Week2 notes",
-  //   url: "week2/index.html"
-  // }
-]
+  }];
+
+// declare variable and begin ordered list
 var linksHtml = "<ol>";
 
 
-var i;
+//loop through links array
 for (var i = 0; i < links.length; i++) {
-
-  // console.log(links[i]['label']);
-
-  var label = links[i]['label'];
-  var url   = links[i]['url']  ;
-  // // label = JSON.stringify(label);
-  // // url = JSON.stringify(url);
+    //assign values to variables coorelated with key.
+    var i;
+    var label = links[i]['label'];
+    var url   = links[i]['url']  ;
+    // create list Item for each item in links array
+    // create anchor tag with label and url
     linksHtml += `<li><a href="${url}">${label}</a></li>`;
-  }
-  linksHtml += "</ol>";
+    }
 
-// console.log(linksHtml);
-document.getElementById('hello').innerHTML = linksHtml;
+//close ordered list
+linksHtml += "</ol>";
 
-// let linkHtml = document.querySelector(".linkHtml");
-// linkHtml.textContent = 'Hello World';
-// var x = document.getElementById("myDIV");
-// x.querySelector(".links").innerHTML = "Hello World!";
-// x.querySelector("#paragraph").innerHTML = "This is a paragraph";
-
-// document.getElementById('hello').innerHTML = "Hello World";
+// insert html into the linksHtml div in index
+document.getElementById('linksHtml').innerHTML = linksHtml;

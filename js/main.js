5a1737ce835e0dcad2ff6ed7166cf9048ed6ed25
Brian Brown <browner86@gmail.com>
2021-05-06 15:43:14 -0400

    add next 6 weeks to JS array.

--- js/main.js
+++ js/main.js
@@ -9,7 +9,19 @@ const links = [
   { label: "Week1 notes",
     url: "week1/index.html"},
   { label: "Week2 notes",
-    url: "week2/index.html"
+    url: "week2/index.html" }
+  { label: "Week3 notes",
+    url: "week3/index.html"},
+  { label: "Week4 notes",
+    url: "week4/index.html"  }
+  { label: "Week5 notes",
+    url: "week5/index.html"},
+  { label: "Week6 notes",
+    url: "week6/index.html" }
+  { label: "Week7 notes",
+    url: "week7/index.html"},
+  { label: "Week8 notes",
+    url: "week8/index.html"
   }];
 
 // declare variable and begin ordered list

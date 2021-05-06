ac312a1fd33e6130d8aa7f0a0e03e45aee1fc116
Brian Brown <browner86@gmail.com>
2021-05-06 15:48:52 -0400

    fix commas in main JS

--- js/main.js
+++ js/main.js
@@ -6,23 +6,14 @@ create a new link to that weeks project.
 //create links array. each iterator is object
 //key value pairs with label and url
 const links = [
-  { label: "Week1 notes",
-    url: "week1/index.html"},
-  { label: "Week2 notes",
-    url: "week2/index.html" }
-  { label: "Week3 notes",
-    url: "week3/index.html"},
-  { label: "Week4 notes",
-    url: "week4/index.html"  }
-  { label: "Week5 notes",
-    url: "week5/index.html"},
-  { label: "Week6 notes",
-    url: "week6/index.html" }
-  { label: "Week7 notes",
-    url: "week7/index.html"},
-  { label: "Week8 notes",
-    url: "week8/index.html"
-  }];
+   { label: "Week1 notes", url: "week1/index.html"}
+  ,{ label: "Week2 notes", url: "week2/index.html"}
+  ,{ label: "Week3 notes", url: "week3/index.html"}
+  ,{ label: "Week4 notes", url: "week4/index.html"}
+  ,{ label: "Week5 notes", url: "week5/index.html"}
+  ,{ label: "Week6 notes", url: "week6/index.html"}
+  ,{ label: "Week7 notes", url: "week7/index.html"}
+  ,{ label: "Week8 notes", url: "week8/index.html"}];
 
 // declare variable and begin ordered list
 var linksHtml = "<ol>";

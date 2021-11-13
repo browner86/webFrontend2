1577d190b6123968ad8b8d82ba4d6a874e9edffe
browner86 <50446300+browner86@users.noreply.github.com>
2021-11-13 11:10:48 -0700

    add weeks 9-13 to root indez and js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,15 @@ const links = [
   ,{ label: "Week5 notes", url: "week5/index.html"}
   ,{ label: "Week6 notes", url: "week6/index.html"}
   ,{ label: "Week7 notes", url: "week7/index.html"}
-  ,{ label: "Week8 notes", url: "week8/index.html"}];
+  ,{ label: "Week8 notes", url: "week8/index.html"}
+  ,{ label: "Week9 notes", url: "week9/index.html"}
+  ,{ label: "Week10 notes", url: "week10/index.html"}
+  ,{ label: "Week11 notes", url: "week11/index.html"}
+  ,{ label: "Week12 notes", url: "week12/index.html"}
+  ,{ label: "Week13 notes", url: "week13/index.html"}
+
+
+];
 
 // declare variable and begin ordered list
 var linksHtml = "<ol>";

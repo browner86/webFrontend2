1b42abb348a8edd086b1e9b5d45dd8f79554143b
browner86 <50446300+browner86@users.noreply.github.com>
2021-11-30 16:18:57 -0700

    add project

--- js/main.js
+++ js/main.js
@@ -19,6 +19,8 @@ const links = [
   ,{ label: "Week11 notes", url: "week11/index.html"}
   ,{ label: "Week12 notes", url: "week12/index.html"}
   ,{ label: "Week13 notes", url: "week13/index.html"}
+  ,{ label: "Project", url: "thereapy/home.html"}
+
 ];
 
 // declare variable and begin ordered list

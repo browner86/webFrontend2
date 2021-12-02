3d8da5b57f561eb17e4c030d2b5b345e1a5d8c5b
browner86 <50446300+browner86@users.noreply.github.com>
2021-12-02 10:52:10 -0700

    fix local storage array.

--- thereapy/getJournalEntries.js
+++ thereapy/getJournalEntries.js
@@ -1,17 +1,37 @@
+// let postArray = localStorage.getItem('postArray');
+// postArray= postArray.split('},')
+// let obj = JSON.parse(postArray[0]);
+// console.log(postArray);
+// console.log(postArray[0]);
+// console.log(obj);
+// let date = ` <p> Date: ${obj.date} </p>`;
+// let title = ` <p> Title: ${obj.title} </p>`;
+// let entry = ` <p> Entry: ${obj.entry} </p>`;
+// let html = `
+// <h5 class="card-header"><img class="avatar-image" src="images/jake-nackos-IF9TK5Uy-KI-unsplash small.jpg" alt="avatar"> ${obj.title}</h5>
+// <div class="card-body">
+//   <h5 class="card-title">${obj.date}</h5>
+//   <p class="card-text">${obj.entry}</p>
+// </div>
+// `
+
+
+// document.getElementById('journal-ent').innerHTML = html;
+
 let postArray = localStorage.getItem('postArray');
-postArray= postArray.split('},')
-let obj = JSON.parse(postArray[0]);
+
+let obj = JSON.parse(postArray);
 console.log(postArray);
 console.log(postArray[0]);
 console.log(obj);
-let date = ` <p> Date: ${obj.date} </p>`;
-let title = ` <p> Title: ${obj.title} </p>`;
-let entry = ` <p> Entry: ${obj.entry} </p>`;
+let date = ` <p> Date: ${obj[0].date} </p>`;
+let title = ` <p> Title: ${obj[0].title} </p>`;
+let entry = ` <p> Entry: ${obj[0].entry} </p>`;
 let html = `
-<h5 class="card-header"><img class="avatar-image" src="images/jake-nackos-IF9TK5Uy-KI-unsplash small.jpg" alt="avatar"> ${obj.title}</h5>
+<h5 class="card-header"><img class="avatar-image" src="images/jake-nackos-IF9TK5Uy-KI-unsplash small.jpg" alt="avatar"> ${obj[0].title}</h5>
 <div class="card-body">
-  <h5 class="card-title">${obj.date}</h5>
-  <p class="card-text">${obj.entry}</p>
+  <h5 class="card-title">${obj[0].date}</h5>
+  <p class="card-text">${obj[0].entry}</p>
 </div>
 `
 
--- thereapy/saveData.js
+++ thereapy/saveData.js
@@ -28,3 +28,33 @@ if(localStorage.getItem('postArray')) {
 }
 }
 
+function sendArrayLocalStorage(){
+    let date = new Date()
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let postDate = `${month}/${day}/${year}.`;
+let storage;
+let postEntry = document.getElementById('entry').value;
+let postTitle = document.getElementById('title').value;
+let obj = { date    :   postDate
+          , title   :   postTitle
+          , entry   :   postEntry };
+// let postObj = JSON.stringify(obj);
+
+if(localStorage.getItem('postArray')) {
+    // console.log("there is a postarray");
+    storage = localStorage.getItem('postArray');
+    let parsed = JSON.parse(storage);
+   
+    parsed.push(obj);
+    let postArrayString = JSON.stringify(parsed);
+    localStorage.setItem('postArray', postArrayString);
+} else{
+    // console.log("there is no postArray");
+    storage = new Array;
+    storage.push(obj);
+    storage = JSON.stringify(storage);
+    localStorage.setItem('postArray', storage);
+}
+}

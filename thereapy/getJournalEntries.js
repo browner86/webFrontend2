79222db9143176078ea06fd5e2c79579a0f14176
browner86 <50446300+browner86@users.noreply.github.com>
2021-12-02 09:49:21 -0700

    add avatar to journal entry.

--- thereapy/getJournalEntries.js
+++ thereapy/getJournalEntries.js
@@ -2,17 +2,16 @@ let postArray = localStorage.getItem('postArray');
 postArray= postArray.split('},')
 let obj = JSON.parse(postArray[0]);
 console.log(postArray);
-console.log(postArray[1]);
+console.log(postArray[0]);
 console.log(obj);
 let date = ` <p> Date: ${obj.date} </p>`;
 let title = ` <p> Title: ${obj.title} </p>`;
 let entry = ` <p> Entry: ${obj.entry} </p>`;
 let html = `
-<h5 class="card-header">${obj.title}</h5>
+<h5 class="card-header"><img class="avatar-image" src="images/jake-nackos-IF9TK5Uy-KI-unsplash small.jpg" alt="avatar"> ${obj.title}</h5>
 <div class="card-body">
   <h5 class="card-title">${obj.date}</h5>
   <p class="card-text">${obj.entry}</p>
-  <a href="#" class="btn btn-primary">Go somewhere</a>
 </div>
 `
 
